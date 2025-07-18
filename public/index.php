fe1643ec63f0aeb3785c168e2352f2e02207c85c
binary-grunt <codegrunt@proton.me>
2025-07-18 16:41:58 +0200

    refactor: Remove unnecessary comments and unused code

--- public/index.php
+++ public/index.php
@@ -11,20 +11,11 @@ error_reporting(E_ALL);
 
 use CapsuleLib\Router\Router;
 use CapsuleLib\Http\SecureHeaders;
-// use CapsuleLib\Lang\Translate;
 
 require dirname(__DIR__) . '/lib/autoload.php';
 
 // 1. En-têtes HTTP sécurisés
 SecureHeaders::send();
-
-# HACK: Calling db
-#$path_db = "/../data/ssapays.db";
-// $db = "sqlite:";
-// $pdo = get_pdo($path_db, $db);
-
-// 2. Traductions (chargement langue + page dynamique)
-// Translate::load(page: 'index'); // ou autre
 
 // 3. Démarrage du routeur
 new Router();
