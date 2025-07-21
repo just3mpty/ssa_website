60c4618dd3af11e962bf4d1c02a4722028f873ce
binary-grunt <codegrunt@proton.me>
2025-07-21 12:30:32 +0200

    docs: Remove outdated content from participer.php

--- templates/pages/participer.php
+++
@@ -1,6 +0,0 @@
-<section class="hero">
-    <div class="container">
-        <h1>Wiki SSA</h1>
-        <p class="slogan">Une base de connaissances collaborative sur la Sécurité Sociale de l’Alimentation.</p>
-    </div>
-</section>
