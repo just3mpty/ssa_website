a191c41d8fe24c64315fb8e1b42f6bdbabef07d0
just3mpty <aurelien.corre@outlook.fr>
2025-07-22 09:55:32 +0200

    ui: updated event component -> added title to event

--- templates/pages/home.php
+++ templates/pages/home.php
@@ -25,12 +25,9 @@
                         <p><?= h_specialchar(substr($event['hours'], 0, 5)) ?></p>
                     </div>
                     <div class="description">
+                        <h3><?= htmlspecialchars($event['titre']) ?></h3>
                         <p><?= nl2br(h_specialchar($event['description'])) ?></p>
                     </div>
-
-                    <?php if (!empty($event['image'])): ?>
-                        <img src="<?= h_specialchar($event['image'], ENT_QUOTES) ?>" alt="Image de l'événement" style="max-width:300px">
-                    <?php endif; ?>
                 </article>
             <?php endforeach; ?>
         <?php endif; ?>
