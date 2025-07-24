9d4ce4354d272e66cb16001a77c5b6abf8c871db
just3mpty <aurelien.corre@outlook.fr>
2025-07-24 16:20:58 +0200

    ui: updated logo svg + project page

--- templates/components/apropos.php
+++ templates/components/apropos.php
@@ -8,25 +8,19 @@
         <div class="text">
             <h2><?= secure_html($str['about_title']) ?></h2>
             <h3><?= secure_html($str['about_subtitle']) ?></h3>
-
             <p><?= secure_html($str['about_intro']) ?></p>
-
             <p><strong><?= secure_html($str['about_axes']) ?></strong></p>
             <ul>
                 <li><a href="https://ulamir-cpie.bzh/" target="_blank">l’ULAMiR CPIE</a>, <?= secure_html($str['about_axes_1']) ?></li>
                 <li><a href="https://www.resam.net/" target="_blank">le RESAM</a>, <?= secure_html($str['about_axes_2']) ?></li>
                 <li><a href="https://buzuk.bzh/" target="_blank">le Buzuk</a>, <?= secure_html($str['about_axes_3']) ?></li>
                 <li><a href="https://www.adess29.fr/faire-reseau/le-pole-du-pays-de-morlaix/" target="_blank">l'ADESS</a> <?= secure_html($str['about_axes_4']) ?></li>
-
             </ul>
-
             <p><strong><?= secure_html($str['about_goal_label']) ?></strong>
                 <?= secure_html($str['about_goal']) ?>
             </p>
-
             <button><?= secure_html($str['about_cta']) ?></button>
         </div>
-
         <div class="illustration">
             <img src="/assets/img/logo.svg" alt="<?= secure_attr($str['about_logo_alt']) ?>">
         </div>
--- templates/pages/projet.php
+++ templates/pages/projet.php
@@ -1,6 +1,41 @@
 <section class="hero">
-    <div class="container">
-        <h1>Wiki SSA</h1>
-        <p class="slogan">Une base de connaissances collaborative sur la Sécurité Sociale de l’Alimentation.</p>
-    </div>
+    <div class="overlay"></div>
+    <h1>Expérimentation d'une caisse de l'alimentation sur le Pays de Morlaix</h1>
+    <p class="slogan">Une base de connaissances collaborative sur la Sécurité Sociale de l’Alimentation.</p>
 </section>
+<div class="background">
+    <section id="about" class="about">
+        <div class="text">
+            <h2>Un premier pas vers une Sécurité Sociale de l'Alimentation</h2>
+            <h3>Pour un droit fondamental et universel à l'alimentation</h3>
+            <p>La Sécurité Sociale de l’Alimentation en Pays de Morlaix est une expérimentation portée par un collectif d’associations et de citoyen.nes pour :</p>
+            <ul>
+                <li>aller vers un droit à l'alimentation pour toutes et tous soutenir les paysan.nes, celles et ceux qui nous nourrissent, tout en prenant soin de notre environnement et de la santé des personnes</li>
+                <li>contribuer à construire un système alimentaire plus juste et plus durable</li>
+                <li>recréer du lien et reprendre du pouvoir d'agir collectivement sur notre territoire</li>
+            </ul>
+
+        </div>
+        <div class="illustration">
+            <img src="/assets/img/logo.svg" alt="<?= secure_attr($str['about_logo_alt']) ?>">
+        </div>
+    </section>
+    <section class="about">
+        <div class="illustration">
+            <img src="/assets/img/logo.svg" alt="<?= secure_attr($str['about_logo_alt']) ?>">
+        </div>
+        <div class="text">
+            <p><strong><?= secure_html($str['about_goal_label']) ?></strong>
+                C'est une initiative locale qui s'inspire d'un projet de société : celui de mettre en place une sécurité sociale de l'alimentation !
+            </p>
+            <p><strong>Plus concrètement, c'est :</strong></p>
+            <ul>
+                <li>un parcours citoyen qui débutera à l’automne 2025, composé chaque mois d’animations, d’ateliers cuisine, de visites de fermes, etc.</li>
+                <li>un budget collectif constitué par les cotisations des membres et des subventionsune gestion démocratique par ses membres, notamment pour le choix des lieux conventionnés</li>
+                <li>une allocation à dépenser chaque mois dans des lieux de distribution alimentaire</li>
+            </ul>
+            <p>Vous pouvez rejoindre le collectif pour participer à la construction de ce beau projet « collectif », n’hésitez pas à nous contacter par mail à ssapaysdemorlaix@mailo.com</p>
+            <button>Rejoindre</button>
+        </div>
+    </section>
+</div>
