b9a66b78967f620e8ce5a4c9dfc0b3dc2128825e
just3mpty <aurelien.corre@outlook.fr>
2025-07-25 11:45:29 +0200

    ui: created career section

--- templates/pages/projet.php
+++ templates/pages/projet.php
@@ -1,11 +1,12 @@
-<section class="hero">
+<section class="hero ssaProject-hero">
     <div class="overlay"></div>
     <h1>Expérimentation d'une caisse de l'alimentation sur le Pays de Morlaix</h1>
     <p class="slogan">Une base de connaissances collaborative sur la Sécurité Sociale de l’Alimentation.</p>
 </section>
+<!-- Section textuelle & explications -->
 <div class="background">
     <section id="about" class="about">
-        <div class="text">
+        <div class="text about-text">
             <h2>Un premier pas vers une Sécurité Sociale de l'Alimentation</h2>
             <h3>Pour un droit fondamental et universel à l'alimentation</h3>
             <p>La Sécurité Sociale de l’Alimentation en Pays de Morlaix est une expérimentation portée par un collectif d’associations et de citoyen.nes pour :</p>
@@ -20,9 +21,9 @@
             <img src="/assets/img/logo.svg" alt="<?= secure_attr($str['about_logo_alt']) ?>">
         </div>
     </section>
-    <section class="about">
-        <div class="illustration">
-            <img src="/assets/img/logo.svg" alt="<?= secure_attr($str['about_logo_alt']) ?>">
+    <section class="about projet-ssa">
+        <div class="illustration projet-ssa-illustration">
+            <img src="/assets/img/banner.webp" alt="<?= secure_attr($str['about_logo_alt']) ?>">
         </div>
         <div class="text">
             <p><strong><?= secure_html($str['about_goal_label']) ?></strong>
@@ -34,8 +35,27 @@
                 <li>un budget collectif constitué par les cotisations des membres et des subventionsune gestion démocratique par ses membres, notamment pour le choix des lieux conventionnés</li>
                 <li>une allocation à dépenser chaque mois dans des lieux de distribution alimentaire</li>
             </ul>
-            <p>Vous pouvez rejoindre le collectif pour participer à la construction de ce beau projet « collectif », n’hésitez pas à nous contacter par mail à ssapaysdemorlaix@mailo.com</p>
+            <p>Vous pouvez rejoindre le collectif pour participer à la construction de ce beau projet « collectif », n’hésitez pas à nous contacter par mail à <a href="mailto:ssapaysdemorlaix@mailo.com"> ssapaysdemorlaix@mailo.com</a></p>
             <button>Rejoindre</button>
         </div>
     </section>
 </div>
+<!-- Section recrutement -->
+<section class="recrutement">
+    <h2>On recrute !</h2>
+    <p>Recherche 100 volontaires pour participer à l'expérimentation Sécurité Sociale de l'Alimentation</p>
+    <p> La campagne de recrutement des 100 personnes qui participeront à la caisse commune est lancé <strong>jusqu’au 20 septembre.</strong>
+        <br> <br>
+        C’est ouvert à toutes et tous, sans conditions de ressources. L’objectif est d’avoir un panel de candidat.e.s le plus représentatif possible de la population du territoire du pays de Morlaix, en terme de niveau de vie et de composition du foyer. Si les candidatures sont trop nombreuses, un tirage au sort sera effectué par catégorie.
+        <br><br>
+        Le dossier de candidature en ligne est à télécharger <a href="">juste ici</a> ou à retrouver sur ce lien : <a href="https://framaforms.org/inscription-ssa-pays-de-morlaix-1750686939" target="_blank">formulaire</a>
+
+        Comment cela se passe concrètement ?
+
+        Les participants auto-déterminent le montant de leur cotisation mensuelle (en se basant sur leur revenu fiscal de référence), et recevront ensuite une allocation en monnaie locale alimentaire (« Buzuk SSA »), à dépenser dans des lieux conventionnés choisis démocratiquement. Chacun cotise selon ses moyens et reçoit 150 buzuks SSA.
+    </p>
+    <h3>Pour plus d’informations</h3>
+    <p>N’hésitez pas à contacter les membres du collectif sur ssapaysdemorlaix@mailo.com
+
+        ou candidature@mailo.com</p>
+</section>
