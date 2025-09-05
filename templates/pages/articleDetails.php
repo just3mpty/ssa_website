559f2ea67e710274c95ff1751d0c6a8b3912c6e7
just3mpty <aurelien.corre@outlook.fr>
2025-09-05 17:03:47 +0200

    ui: added new data to articleDetails.php

--- templates/pages/articleDetails.php
+++ templates/pages/articleDetails.php
@@ -1,6 +1,11 @@
 <section class="details">
-    <div style="margin-top: 200px;">
-        <h1>Testestsest</h1>
-        <p><?= $article->titre ?></p>
+    <div style="margin-top: 120px;">
+        <h1><?= secure_html($article->titre) ?></h1>
+        <h2><?= secure_html($article->resume) ?></h2>
+        <p><?= secure_html($article->description) ?></p>
+        <p>Date : <?= secure_html($article->date_event) ?></p>
+        <p>Lieu : <?= secure_html($article->lieu) ?></p>
+        <div class="separator"></div>
+        <p>Article rédigé par : <?= secure_html($article->author) ?></p>
     </div>
 </section>
