2b464228333ede36635df1377fc2e4dc31aaa101
trollheap <codegrunt@proton.me>
2025-09-25 09:15:35 +0200

    style: add strict types declaration to MariaDBConnection and SqliteConnection

--- src/Database/MariaDBConnection.php
+++ src/Database/MariaDBConnection.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Capsule\Database;
 
 use PDO;
--- src/Database/SqliteConnection.php
+++ src/Database/SqliteConnection.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Capsule\Database\Connection;
+namespace Capsule\Database;
 
 use PDO;
 use PDOException;
